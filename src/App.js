22e53f5ac296a3b1d0bcfd1a5d95e92085bf2acc
Halim Tekin <thehalimtekin@gmail.com>
2020-01-03 01:14:00 +0300

    edited app.kd

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React Öğrenmeye Başla
         </a>
       </header>
     </div>
