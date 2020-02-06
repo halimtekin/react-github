325b969ae28aaf8951cec162e431cf46c0568ba6
Halim Tekin <thehalimtekin@gmail.com>
2020-02-07 00:31:22 +0300

    gitlens ext test

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Şimdi React.js Öğrenmeye Başlayın.
+          Şimdi React.js Öğrenmeye Başla.
         </a>
       </header>
     </div>
